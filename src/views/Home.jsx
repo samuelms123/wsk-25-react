aba2aa7ca2d26e6e7f9cab01ffa3ec32de7abec1
Samuelms <samuel.sarimo@gmail.com>
2025-04-09 11:39:22 +0300

    Add owner username to media list

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -10,8 +10,21 @@ const Home = () => {
 
   const getMedia = async () => {
     try {
-      const data = await fetchData(import.meta.env.VITE_MEDIA_API + '/media');
-      setMediaArray(data);
+      const mediaData = await fetchData(
+        import.meta.env.VITE_MEDIA_API + '/media',
+      );
+
+      const authApiUrl = import.meta.env.VITE_AUTH_API;
+      const newData = await Promise.all(
+        mediaData.map(async (item) => {
+          const data = await fetchData(`${authApiUrl}/users/${item.user_id}`);
+          return {...item, username: data.username};
+        }),
+      );
+
+      console.log('usersData: ', newData);
+
+      setMediaArray(newData);
     } catch (error) {
       console.error('Error fetching media:', error);
     }
@@ -30,6 +43,7 @@ const Home = () => {
             <th>Thumbnail</th>
             <th>Title</th>
             <th>Description</th>
+            <th>Owner</th>
             <th>Created</th>
             <th>Size</th>
             <th>Type</th>
